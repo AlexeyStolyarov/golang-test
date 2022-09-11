72984a26e620ede2ca2e4058dab86487f4e9334b
Aleksey Stolyarov <deprecated@mail.ru>
2022-09-11 03:30:53 +0300

    context

--- src/6.context/context.go
+++ src/6.context/context.go
@@ -3,44 +3,60 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 var (
 	myRand = rand.New(rand.NewSource(time.Now().UnixNano()))
-	orders = make(chan string)
-	taxi   = []string{
-		"yandex",
-		"deli",
-		"uber",
-	}
 )
 
 func main() {
+	var (
+		ordersCh = make(chan string)
+		taxi     = []string{"yandex", "deli", "uber", "SberTaxi"}
+		winner   string
+		wg       sync.WaitGroup
+	)
+
 	ctx, cancel := context.WithCancel(context.Background())
 	// timeout := time.After(10 * time.Second)
 
 	defer cancel()
 	// rand.Seed(time.Now().UnixNano())
 
-	for _, i := range taxi {
-		// fmt.Println(i)
-		go call(ctx, i)
+	// doesn't work in that way!!
+	// for _, i := range taxi {
+	// fmt.Println(i)
+	for i := range taxi {
+		tt := taxi[i]
+
+		wg.Add(1)
+
+		go func() {
+			// doesn't work in that way!!
+			// go call(ctx, tt, ordersCh)
+			call(ctx, tt, ordersCh)
+			wg.Done()
+		}()
 	}
 
 	// cancel()
+	go func() {
+		winner = <-ordersCh
+		cancel()
+	}()
 
-	order := <-orders
-	fmt.Println("main: Got order from: " + order)
-	cancel()
-
+	wg.Wait()
+	log.Printf("found car in %q", winner)
 	fmt.Println("main: quit!!")
 
 }
 
-func call(ctx context.Context, t string) {
-	defer fmt.Println(t + " says: bye!!")
+func call(ctx context.Context, t string, ordersCh chan string) {
+	time.Sleep(2 * time.Second)
 
 	for {
 		select {
@@ -48,12 +64,14 @@ func call(ctx context.Context, t string) {
 			fmt.Println(t + "says: timeout!")
 			return
 		case <-ctx.Done():
-			fmt.Print(ctx.Err())
+			fmt.Println(t + " says: bye!! ->" + ctx.Err().Error())
+			return
 		default:
-			time.Sleep(time.Duration(myRand.Intn(10)) * time.Second)
-			fmt.Println("calling for " + t)
-			orders <- t
-
+			time.Sleep(time.Millisecond)
+			if rand.Float64() > 0.75 {
+				ordersCh <- t
+				return
+			}
 		}
 
 	}
