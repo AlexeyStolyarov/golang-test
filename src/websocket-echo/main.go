d100f4081f3a91aeb2651f758ff16b2baf58429b
Aleksey Stolyarov <deprecated@mail.ru>
2022-10-03 00:40:27 +0300

    WIP: WS

--- src/websocket-echo/main.go
+++ src/websocket-echo/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"os/signal"
+	"sync"
 
-	"websocket-echo/config"
+	"websocket-echo/server"
 )
 
 func main() {
-	var c config.Conf
+	// var c config.Conf
+	var wg sync.WaitGroup
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
 
-	c.GetConf(config.File)
+	// c.GetConf(config.File)
 
-	fmt.Printf("%+v\n", c)
+	// fmt.Printf("%+v\n", c)
+
+	wg.Add(1)
+	go server.Ws_server(ctx, &wg)
+
+	wg.Wait()
 }
--- src/websocket-echo/server/main.go
+++ src/websocket-echo/server/main.go
@@ -1,1 +1,74 @@
 package server
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+func Ws_server(ctx context.Context, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	log.SetFlags(log.LstdFlags | log.LUTC)
+
+	locations := []string{"Home", "Work", "Camphous", "Coworking"}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		traceID := genId()
+		log.Printf("%v: Receive request\n", traceID)
+
+		log.Printf("%v: Print header\n", traceID)
+		for k := range r.Header {
+			log.Printf("%v: header[%v]:%v\n", traceID, k, r.Header.Get(k))
+		}
+		log.Printf("%v: Read body by 10 bytes\n", traceID)
+
+		chunk := make([]byte, 10)
+		for i := 0; true; i++ {
+			n, err := r.Body.Read(chunk)
+			log.Printf("%v: %v index. read %d bytes '%s' err: %v\n", traceID, i, n, chunk, err)
+			if err == io.EOF {
+				break
+			}
+		}
+		log.Printf("%v: Finish read body\n", traceID)
+
+		header := w.Header()
+		UserNameValue := "tochka"
+		if r.Header.Get("User-Name-Data") != "" {
+			UserNameValue = "authorizated user name is " + r.Header.Get("User-Name-Data")
+		}
+		header.Add("User-Id", "12341")
+		header.Add("User-Name", UserNameValue)
+		header.Add("User-Locations", strings.Join(locations, ";"))
+
+		for _, l := range locations {
+			header.Add("Location-"+l, strings.Join([]string{"read", "write"}, ";"))
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Println("Start listening...")
+	go http.ListenAndServe(":8080", h)
+	<-ctx.Done()
+
+	fmt.Println("Gracefull shtd")
+}
+
+func genId() string {
+	var b [16]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(b[:])
+}
